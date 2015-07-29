0e3cb7d6202810a24d1915906a5592dd99a3d2a6
Simon <simon.ayoub@gmail.com>
2015-07-30 00:21:12 +1000

    Internet Permissions
    added internet permissions

--- app/src/main/java/com/sayo/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/sayo/android/sunshine/ForecastFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sayo.android.sunshine;
 
 import android.os.AsyncTask;
@@ -23,13 +38,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 /**
- * A placeholder fragment containing a simple view.
+ * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
  */
 public class ForecastFragment extends Fragment {
 
-    public ForecastFragment() { }
+    private ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,6 +67,8 @@ public class ForecastFragment extends Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -58,58 +77,44 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23?- Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        // Now that we have some dummy forecast data, create an ArrayAdapter.
+        // The ArrayAdapter will take data from a source (like our dummy forecast) and
+        // use it to populate the ListView it's attached to.
+        mForecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_forcast, // The name of the layout ID.
+                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                        weekForecast);
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        // create some fake data to populate ListView
-        String[] forcastArray = {
-                "Today - Sunny - 32",
-                "Tomorrow - Foggy - 24",
-                "Wed - Cloudy - 32",
-                "Thurs - Sunny - 32",
-                "Fri - Sunny - 32",
-                "Sat - Sunny - 32",
-                "Sun - Sunny - 32",
-        };
-        List<String> weekForcecast = new ArrayList<String>(Arrays.asList(forcastArray));
-
-        // Create an adapter which is aware of the context
-        ArrayAdapter<String> mForecastAdapter =
-                new ArrayAdapter<String>(
-                        //context
-                        getActivity(),
-                        //id of list item
-                        R.layout.list_item_forcast,
-                        //textview to populate
-                        R.id.list_item_forecast_textview,
-                        //data
-                        weekForcecast);
-
-        // Obtain the reference of the listview_forecast to bind with adapter
-        ListView listView =
-                (ListView)rootView.findViewById(R.id.listview_forecast);
-
-
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-
-        new FetchWeatherTask().execute("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
 
         return rootView;
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String>
-    {
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
-        /** The system calls this to perform work in the UI thread and delivers
-         * the result from doInBackground() */
         @Override
-        protected void onPostExecute(String result) {
-
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
+        protected Void doInBackground(Void... params) {
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
@@ -122,7 +127,7 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL(params[0]);
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -151,12 +156,14 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+
+                Log.v("ForecastFragment.java", forecastJsonStr);
             } catch (IOException e) {
-                Log.e("ForecastFragment", "Error ", e);
+                Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
                 // to parse it.
                 return null;
-            } finally{
+            } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
@@ -164,12 +171,11 @@ public class ForecastFragment extends Fragment {
                     try {
                         reader.close();
                     } catch (final IOException e) {
-                        Log.e("ForecastFragment", "Error closing stream", e);
+                        Log.e(LOG_TAG, "Error closing stream", e);
                     }
                 }
             }
-
-            return forecastJsonStr;
+            return null;
         }
     }
 }
